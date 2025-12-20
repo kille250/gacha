9ce2bb5516a1d04baf571f7ab8322e93d4df89ba
Moistdio <Dionyssioss@gmail.com>
2025-12-20 11:00:25 +0100

    Remove legacy code

--- frontend/src/components/Gacha/index.js
+++
@@ -1,9 +0,0 @@
-// Main exports for Gacha components
-export * from './theme';
-export * from './animations';
-export * from './CharacterCard';
-export * from './MultiPullMenu';
-export * from './ResultsDisplay';
-export * from './RollControls';
-export * from './Layout';
-
